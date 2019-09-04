9aca3eb147ca1241cbd4a400201fef38b1da8a96
Marie  Pepe <mariepepe@Maries-MacBook-Air.local>
2019-09-04 10:14:51 -0400

    Updates

---
+++ precache-manifest.d21455a5ee8b472b444ee4b96ae541eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d25faa8867ebb2f1790f7358e98a94a0",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "06f25edc1e1319d85fea",
+    "url": "/clickygame/static/css/main.1cdb9bc6.chunk.css"
+  },
+  {
+    "revision": "f2729b5fdf3fe2f7cfd1",
+    "url": "/clickygame/static/js/2.a7a17e53.chunk.js"
+  },
+  {
+    "revision": "06f25edc1e1319d85fea",
+    "url": "/clickygame/static/js/main.2b523b1b.chunk.js"
+  },
+  {
+    "revision": "cd9a1e80c368f6151018",
+    "url": "/clickygame/static/js/runtime~main.0fa0c129.js"
+  }
+]);
